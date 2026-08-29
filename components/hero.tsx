b82b116e5502bd590cd8c9bbb45708c3ac9ff282
AyeshaChaudhry15 <ayesharehmat6789@gmail.com>
2026-08-29 21:08:28 +0500

    fixed

--- components/hero.tsx
+++ components/hero.tsx
@@ -15,11 +15,11 @@ export default function Hero() {
     <section className="bg-[#06111A] px-5 lg:px-8 w-full">
       <div className="mx-auto grid max-w-7xl items-center gap-2 lg:grid-cols-2">
         <div>
-          <h1 className="text-6xl font-bold text-white sm:text-6xl">
+          <h1 className="text-4xl font-bold text-white sm:text-5xl lg:text-6xl">
             The Future <br />
             Moves <span className="text-lime-400">Electric</span>
           </h1>
-          <p className="mt-6 max-w-md text-gray-400 text-xl">
+          <p className="mt-6 max-w-md text-gray-400 text-base sm:text-xl">
             Explore 10+ Trusted Brands, 50+ Models. One Destination.
           </p>
           <div className="mt-8 flex flex-wrap gap-4">
@@ -32,7 +32,7 @@ export default function Hero() {
           </div>
         </div>
 
-        <div className="relative h-[380px] w-full sm:h-[480px] lg:h-[500px] lg:-ml-8 lg:scale-125">
+        <div className="relative mt-8 h-[280px] w-full sm:h-[380px] md:h-[480px] lg:mt-0 lg:h-[500px] lg:-ml-8 lg:scale-125">
           <Image
             src="/hero1.webp"
             alt="Electric motorbike"
@@ -43,13 +43,13 @@ export default function Hero() {
         </div>
       </div>
 
-      <div className="mx-auto grid max-w-7xl grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 h-25 ">
+      <div className="mx-auto grid max-w-7xl grid-cols-1 gap-3 py-8 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 lg:gap-0 lg:py-0 lg:h-25">
         {STATS.map(({ icon: Icon, label }) => (
           <div
             key={label}
-            className="flex items-center gap-3 rounded-lg border border-white/5 bg-white/[0.03] px-4 py-4 "
+            className="flex items-center gap-3 rounded-lg border border-white/5 bg-white/[0.03] px-4 py-4"
           >
-            <Icon className=" text-lime-400" size={30} />
+            <Icon className="text-lime-400" size={30} />
             <span className="text-lg font-medium text-gray-200">{label}</span>
           </div>
         ))}
