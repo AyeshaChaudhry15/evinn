8d802d140aacc0444dca7ae7ee2cc4111313c1c5
AyeshaChaudhry15 <ayesharehmat6789@gmail.com>
2026-09-04 17:16:30 +0500

    images

--- components/brands.tsx
+++ components/brands.tsx
@@ -103,7 +103,7 @@ const brands: Brand[] = [
 export default function Brands() {
   return (
     <main className="min-h-screen overflow-x-hidden bg-[#081019] py-5 sm:px-2  lg:h-screen lg:overflow-hidden">
-      <div className="mx-auto max-w-[1250px] pt-10">
+      <div className="mx-auto max-w-[1300px] pt-10">
         <div className="mb-5">
           <h1 className="text-4xl font-bold text-[#E8EBF2] sm:text-5xl ">
             All Brands
@@ -121,7 +121,7 @@ export default function Brands() {
               href={brand.link}
               className="
                 flex
-                h-[90px]
+                h-[100px]
                 items-center
                 justify-center
                 rounded-lg
